6cd60655319595cacee9befbbeb3d0c05169f227
bacon1097 <brunyeeb@gmail.com>
2022-05-20 22:02:41 +0100

    more pricing

--- globals.tsx
+++ globals.tsx
@@ -31,96 +31,118 @@ export const PHONE_PRICING: PhonePricingMapping = {
     screen: 40,
     battery: 30,
     "back camera": 20,
+    "front camera": 10,
   },
   "iPhone 6 Plus": {
     screen: 40,
     battery: 30,
     "back camera": 20,
+    "front camera": 10,
   },
   "iPhone 6s": {
     screen: 40,
     battery: 30,
     "back camera": 20,
+    "front camera": 10,
   },
   "iPhone 6s Plus": {
     screen: 40,
     battery: 30,
     "back camera": 20,
+    "front camera": 10,
   },
   "iPhone 7": {
     screen: 50,
     battery: 30,
     "back camera": 20,
+    "front camera": 15,
   },
   "iPhone 7 Plus": {
     screen: 50,
     battery: 30,
     "back camera": 20,
+    "front camera": 15,
   },
   "iPhone 8": {
     screen: 50,
     battery: 20,
     "back camera": 20,
+    "front camera": 15,
   },
   "iPhone 8 Plus": {
     screen: 50,
     battery: 30,
     "back camera": 20,
+    "front camera": 15,
   },
   "iPhone X": {
     screen: 90,
     battery: 30,
     "back camera": 70,
+    "front camera": 15,
   },
   "iPhone XR": {
     screen: 95,
     battery: 35,
     "back camera": 85,
+    "front camera": 15,
   },
   "iPhone XS": {
     screen: 250,
     "back camera": 95,
+    "front camera": 15,
   },
   "iPhone XS Max": {
     screen: 250,
     "back camera": 95,
+    "front camera": 20,
   },
   "iPhone 11": {
     screen: 250,
     "back camera": 95,
+    "front camera": 25,
   },
   "iPhone 11 Pro": {
     screen: 250,
     "back camera": 150,
+    "front camera": 35,
   },
   "iPhone 11 Pro Max": {
     screen: 250,
     "back camera": 150,
+    "front camera": 35,
   },
   "iPhone 12 mini": {
     screen: 250,
+    "back camera": 105,
   },
   "iPhone 12": {
     screen: 250,
+    "back camera": 95,
   },
   "iPhone 12 Pro": {
     screen: 250,
+    "back camera": 175,
   },
   "iPhone 12 Pro Max": {
     screen: 290,
-    "back camera": 170,
+    "back camera": 160,
   },
   "iPhone 13 mini": {
     screen: 250,
+    "back camera": 105,
   },
   "iPhone 13": {
     screen: 250,
+    "back camera": 105,
   },
   "iPhone 13 Pro": {
     screen: 250,
+    "back camera": 160,
   },
   "iPhone 13 Pro Max": {
     screen: 250,
+    "back camera": 160,
   },
 };
 
