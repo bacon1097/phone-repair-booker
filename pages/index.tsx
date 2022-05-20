8a8da3ff27598f3c4103001fdd1af4f43ad5f9c3
bacon1097 <brunyeeb@gmail.com>
2022-05-20 17:04:55 +0100

    home page animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Button from "../components/Button";
@@ -9,16 +10,45 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <Title className={styles.title}>What are you looking for?</Title>
+      <motion.div
+        initial={{
+          y: -50,
+        }}
+        animate={{
+          opacity: [0, 1],
+          y: 0,
+          transition: {
+            type: "spring",
+            duration: 0.4,
+            mass: 1,
+            stiffness: 100,
+            damping: 5,
+          },
+        }}
+      >
+        <Title className={styles.title}>What are you looking for?</Title>
+      </motion.div>
       <div className={styles.btnContainer}>
-        <Button type="default">
-          Give Phone Repairs
-        </Button>
-        <Button type="cta" onClick={() => {
-          router.push("/book-repair");
-        }}>
-          Get Phone Repaired
-        </Button>
+        <Button type="default">Give Phone Repairs</Button>
+        <motion.div
+          animate={{
+            rotate: [0, -3, 3, 0],
+            transition: {
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: 2,
+            },
+          }}
+        >
+          <Button
+            type="cta"
+            onClick={() => {
+              router.push("/book-repair");
+            }}
+          >
+            Get Phone Repaired
+          </Button>
+        </motion.div>
       </div>
       <img src={"./curly-arrow.svg"} alt="arrow" width={250} height={250} />
     </div>
